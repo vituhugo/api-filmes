a56d58002453a5c204268749abe88f431d9a2c15
Victor Rodrigues <victor.rodrigues135@gmail.com>
2020-10-09 20:33:39 -0300

    first commitg

---
+++ models/Filme.js
@@ -0,0 +1,29 @@
+module.export = (sequelize, DataTypes) => {
+    const Filme = sequelize.define("Filme",{
+        titulo: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        avaliacao: {
+            type: DataTypes.FLOAT,
+            allowNull: false
+        },
+        premios: {
+            type: DataTypes.INTEGER,
+            unsigned: true
+        },
+        data_lancamento: {
+            type: DataTypes.DATE,
+            allowNull: false
+        },
+        duracao: {
+            type: DataTypes.INTEGER,
+            unsigned: true
+        },
+        genero_id: {
+            type: DataTypes.INTEGER,
+            unsigned: true
+        }
+    },{timestamps: false, tableName: "filmes"}); // created_at e updated_at
+    return Filme;
+}
