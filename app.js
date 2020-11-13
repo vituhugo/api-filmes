1e5d2b254c2e0557353a42fbac8af2ff08dfb101
Victor Rodrigues <victor.rodrigues135@gmail.com>
2020-11-13 21:29:29 +0000

    api

--- app.js
+++ app.js
@@ -3,11 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var filmesRouter = require('./routes/filmes');
-var generoRouter = require('./routes/generos');
+var indexRouter = require('./routes/index')
 
 var app = express();
 
@@ -22,9 +18,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/filmes', filmesRouter);
-app.use('/generos', generoRouter)
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/generos.js
+++
@@ -1,7 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-var GeneroController = require('./../controllers/GeneroController');
-router.get('/', GeneroController.home)
-
-module.exports = router;
